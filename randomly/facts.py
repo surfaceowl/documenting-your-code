d497a0801bf53939832406e50d28777ab4e2deac
surfaceowl <chris@surfaceowl.com>
2022-04-28 14:44:36 -0600

    add typing and docstrings to random facts function
    - add typing and docstrings
    - correct json quoting to use double-quotes
    - add example api output file
    - add python_scratchfile for experimentation

--- randomly/facts.py
+++ randomly/facts.py
@@ -1,8 +1,20 @@
 import requests
 from requests.exceptions import RequestException
+from typing import Tuple, Union, Dict
+import json
 
 
-def generate_random_fact(output_format, language):
+def generate_random_fact(output_format: str, language: str) -> Union[Dict, str]:
+    """
+    queries uselessfacts api for random fact; returns JSON if available - else string
+    Args:
+        output_format: json or str
+        language: "en" or "de" (English/German)
+
+    Returns: json or str
+
+    """
+
     if language not in {"en", "de"}:
         raise ValueError(f"{language} is not supported.")
 
@@ -10,8 +22,7 @@ def generate_random_fact(output_format, language):
         raise ValueError(f"{output_format} is not supported.")
 
     response = requests.get(
-        f"https://uselessfacts.jsph.pl/random.{output_format}?language={language}"
-    )
+        f"https://uselessfacts.jsph.pl/random.{output_format}?language={language}")
 
     if response.status_code == 200:
         if output_format == "json":
@@ -20,7 +31,6 @@ def generate_random_fact(output_format, language):
             fact = response.text
     else:
         raise RequestException(
-            f"Something went wrong. Request returned status {response.status_code}."
-        )
-
-    return fact
+            f"Something went wrong. Request returned status {response.status_code}.")
+    # api returns dict with single quotes; convert to double-quote match json standard
+    return json.dumps(fact)
---
+++ randomly_scratchfile.py
@@ -0,0 +1,5 @@
+from randomly.facts import generate_random_fact
+
+print(generate_random_fact(output_format="json", language="en"))
+
+
